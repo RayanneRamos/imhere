b0abbfe651aaac238f13fc38276c0b6e3b628cce
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-07-06 16:42:54 -0300

    importando o componente Participant

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,6 @@
 import { Text, TextInput, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
+import { Participant } from "../../components/Participant";
 
 export function Home() {
   function handleParticipantAdd() {
@@ -20,6 +21,10 @@ export function Home() {
           <Text style={styles.buttonText}>+</Text>
         </TouchableOpacity>
       </View>
+      <Participant />
+      <Participant />
+      <Participant />
+      <Participant />
     </View>
   );
 }
